83e19a1de5f8744b2872a12e80f354fc3fdbbba9
ViaBot <viabot@ductworks.io>
2026-05-13 06:40:55 +0000

    Update from airbnb/viaduct at release/v1.0.0
    GitOrigin-RevId: ae1c537f2fb87faa58576dad36dbbeb4e984130e

--- src/main/kotlin/com/example/resolvers/HelloWorldResolvers.kt
+++ src/main/kotlin/com/example/resolvers/HelloWorldResolvers.kt
@@ -2,19 +2,15 @@
 package com.example.viadapp.resolvers
 
 import com.example.viadapp.resolvers.resolverbases.QueryResolvers
-import viaduct.api.Resolver
+import viaduct.api.resolver.Resolver
 
 // tag::greeting-resolver[6] How to create a resolver
 @Resolver
 class GreetingResolver : QueryResolvers.Greeting() {
-    override suspend fun resolve(ctx: Context): String {
-        return "Hello, World!"
-    }
+    override suspend fun resolve(ctx: Context) = "Hello, World!"
 }
 
 @Resolver
 class AuthorResolver : QueryResolvers.Author() {
-    override suspend fun resolve(ctx: Context): String {
-        return "Brian Kernighan"
-    }
+    override suspend fun resolve(ctx: Context) = "Brian Kernighan"
 }
